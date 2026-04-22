21773be136232a80d61de7f8216d7306038d7600
Soner Peker <sonerpeker84@gmail.com>
2026-04-22 16:11:14 +0300

    Using Sytem and some int Function

--- CSharpBook/Chapter2/Program.cs
+++ CSharpBook/Chapter2/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+System.Console.WriteLine("Hello, World!");
+
+int x = 12*30;
+System.Console.WriteLine($"The value of x is {x}");
+
+
+
+int y = 12;
+int z = 30;
+int t = y * z;
+Console.WriteLine($"The value of {t}");
+
+
+
+Console.WriteLine(FeetToInches(30));
+Console.WriteLine(FeetToInches(1200));
+
+int FeetToInches(int feet)
+{
+    int inches=feet*12;
+    return inches;
+}
+
+
+
+SayHello();
+void SayHello()
+{
+    Console.WriteLine("Hello Soner");
+}
